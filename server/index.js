e79bfa6abf484af7ea63dabe5a2945b2a7423c15
Sean Spradlin <seanspradlin@gmail.com>
2016-09-18 17:03:42 -0500

    added helmet

--- server/index.js
+++ server/index.js
@@ -3,14 +3,16 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const routes = require('/routes');
+const routes = require('./routes');
 const config = require('./config');
 const winston = require('winston');
+const helmet = require('helmet');
 
 const app = express();
 
 mongoose.connect(config.db.url);
 
+app.use(helmet);
 app.use(express.static(path.resolve('client')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
