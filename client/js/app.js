c095f697411fd8c2e4ff99a823fff1e9106fd9ff
Sean Spradlin <seanspradlin@gmail.com>
2016-09-21 19:17:13 -0500

    Started adding bootstrap

--- client/js/app.js
+++ client/js/app.js
@@ -1,64 +1,64 @@
 /* global $ jQuery */
 
 'use strict';
-function SectionManager() {
-  this._sections = [
-    '#nameSection',
-    '#addressSection',
-    '#contactSection',
-    '#membersSection',
-  ];
+// function SectionManager() {
+//   this._sections = [
+//     '#nameSection',
+//     '#addressSection',
+//     '#contactSection',
+//     '#membersSection',
+//   ];
 
-  this._currentSection = 0;
-  this._progress = $('footer');
-  this.hideOtherSections();
-}
+//   this._currentSection = 0;
+//   this._progress = $('footer');
+//   this.hideOtherSections();
+// }
 
-SectionManager.prototype.updateProgressBar =
-  function updateProgressBar() {
-    const progress = `${(this._currentSection / this._sections.length) * 100}%`;
-    $('footer').animate({ width: progress }, 400);
-  };
+// SectionManager.prototype.updateProgressBar =
+//   function updateProgressBar() {
+//     const progress = `${(this._currentSection / this._sections.length) * 100}%`;
+//     $('footer').animate({ width: progress }, 400);
+//   };
 
-SectionManager.prototype.current =
-  function current() {
-    return $(this._sections[this._currentSection]);
-  };
+// SectionManager.prototype.current =
+//   function current() {
+//     return $(this._sections[this._currentSection]);
+//   };
 
-SectionManager.prototype.hideOtherSections =
-  function hideOtherSections() {
-    this.current().show().siblings().hide();
-  };
+// SectionManager.prototype.hideOtherSections =
+//   function hideOtherSections() {
+//     this.current().show().siblings().hide();
+//   };
 
-SectionManager.prototype.prev =
-  function prev(fn) {
-    if (this._currentSection > 0) {
-      this.current().fadeOut(400, () => {
-        this._currentSection--;
-        this.updateProgressBar();
-        this.current().fadeIn(400, fn);
-      });
-    }
-  };
+// SectionManager.prototype.prev =
+//   function prev(fn) {
+//     if (this._currentSection > 0) {
+//       this.current().fadeOut(400, () => {
+//         this._currentSection--;
+//         this.updateProgressBar();
+//         this.current().fadeIn(400, fn);
+//       });
+//     }
+//   };
 
-SectionManager.prototype.next =
-  function next(fn) {
-    if (this._currentSection < (this._sections.length - 1)) {
-      this.current().fadeOut(400, () => {
-        this._currentSection++;
-        this.updateProgressBar();
-        this.current().fadeIn(400, fn);
-      });
-    }
-  };
+// SectionManager.prototype.next =
+//   function next(fn) {
+//     if (this._currentSection < (this._sections.length - 1)) {
+//       this.current().fadeOut(400, () => {
+//         this._currentSection++;
+//         this.updateProgressBar();
+//         this.current().fadeIn(400, fn);
+//       });
+//     }
+//   };
 
-$(document).ready(() => {
-  const manager = new SectionManager();
-  $('#btnBack').click(() => {
-    manager.prev();
-  });
-  $('#btnNext').click(() => {
-    manager.next();
-  });
-});
+// $(document).ready(() => {
+//   const manager = new SectionManager();
+//   $('#btnBack').click(() => {
+//     manager.prev();
+//   });
+//   $('#btnNext').click(() => {
+//     manager.next();
+//   });
+// });
 
