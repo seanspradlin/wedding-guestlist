f84b256312979bb3d791dda715120f5ab08afbd4
Sean Spradlin <seanspradlin@gmail.com>
2016-09-22 22:12:53 -0500

    made form fade in

--- client/js/app.js
+++ client/js/app.js
@@ -1,64 +1,23 @@
 /* global $ jQuery */
 
 'use strict';
-// function SectionManager() {
-//   this._sections = [
-//     '#nameSection',
-//     '#addressSection',
-//     '#contactSection',
-//     '#membersSection',
-//   ];
+class FormManager {
+  constructor() {
+    this.currentSection = $('#householdForm section:first');
+    this.currentSection.show();
+  }
 
-//   this._currentSection = 0;
-//   this._progress = $('footer');
-//   this.hideOtherSections();
-// }
-
-// SectionManager.prototype.updateProgressBar =
-//   function updateProgressBar() {
-//     const progress = `${(this._currentSection / this._sections.length) * 100}%`;
-//     $('footer').animate({ width: progress }, 400);
-//   };
-
-// SectionManager.prototype.current =
-//   function current() {
-//     return $(this._sections[this._currentSection]);
-//   };
-
-// SectionManager.prototype.hideOtherSections =
-//   function hideOtherSections() {
-//     this.current().show().siblings().hide();
-//   };
-
-// SectionManager.prototype.prev =
-//   function prev(fn) {
-//     if (this._currentSection > 0) {
-//       this.current().fadeOut(400, () => {
-//         this._currentSection--;
-//         this.updateProgressBar();
-//         this.current().fadeIn(400, fn);
-//       });
-//     }
-//   };
-
-// SectionManager.prototype.next =
-//   function next(fn) {
-//     if (this._currentSection < (this._sections.length - 1)) {
-//       this.current().fadeOut(400, () => {
-//         this._currentSection++;
-//         this.updateProgressBar();
-//         this.current().fadeIn(400, fn);
-//       });
-//     }
-//   };
-
-// $(document).ready(() => {
-//   const manager = new SectionManager();
-//   $('#btnBack').click(() => {
-//     manager.prev();
-//   });
-//   $('#btnNext').click(() => {
-//     manager.next();
-//   });
-// });
+  next(fn) {
+    if (!!this.currentSection.next()[0]) {
+      this.currentSection.fadeOut(400, () => {
+        this.currentSection = this.currentSection.next();
+        this.currentSection.fadeIn(400, fn || (() => {}));
+      });
+    }
+  }
+}
+let form;
+$(document).ready(() => {
+  form = new FormManager();
+});
 
