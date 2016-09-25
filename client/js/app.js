9fd2abbcbfe754b53983120ea26837ffff84fe41
Sean Spradlin <seanspradlin@gmail.com>
2016-09-24 19:34:23 -0500

    make for es5

--- client/js/app.js
+++ client/js/app.js
@@ -1,23 +1,32 @@
 /* global $ jQuery */
 
 'use strict';
-class FormManager {
-  constructor() {
-    this.currentSection = $('#householdForm section:first');
-    this.currentSection.show();
-  }
 
-  next(fn) {
-    if (!!this.currentSection.next()[0]) {
-      this.currentSection.fadeOut(400, () => {
-        this.currentSection = this.currentSection.next();
-        this.currentSection.fadeIn(400, fn || (() => {}));
-      });
-    }
+function FormManager() {
+  this.currentSection = $('#householdForm section:first');
+  this.currentSection.show();
+}
+
+FormManager.prototype.next = function next(fn) {
+  if (!!this.currentSection.next()[0]) {
+    this.currentSection.fadeOut(400, () => {
+      this.currentSection = this.currentSection.next();
+      this.currentSection.fadeIn(400, fn || (() => { }));
+    });
   }
 }
+
+FormManager.prototype.prev = function prev(fn) {
+  if (!!this.currentSection.prev()[0]) {
+    this.currentSection.fadeOut(400, () => {
+      this.currentSection = this.currentSection.prev();
+      this.currentSection.fadeIn(400, fn || (() => { }));
+    })
+  }
+}
+
 let form;
-$(document).ready(() => {
+$(document).ready(function () {
   form = new FormManager();
 });
 
