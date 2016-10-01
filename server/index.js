dc0384cdbe0527650d889f74bfe6c4f31c2af958
Sean Spradlin <seanspradlin@gmail.com>
2016-10-01 00:13:38 -0500

    Expose only route in index

--- server/index.js
+++ server/index.js
@@ -3,20 +3,31 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const routes = require('./routes');
 const config = require('./config');
 const winston = require('winston');
 const helmet = require('helmet');
+const Household = require('./models').Household;
 
 const app = express();
 
 mongoose.connect(config.db.url);
 
 app.use(express.static(path.resolve('client')));
-app.use(helmet);
+app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/api', routes);
+app.post('/household', (req, res) => {
+  const household = new Household(req.body);
+  household.save(e => {
+    if (e) {
+      winston.error(e);
+      res.status(400);
+      res.end();
+    } else {
+      res.json(household);
+    }
+  });
+});
 
 app.listen(config.port, () => {
   winston.info(`Server is running on port ${config.port}`);
